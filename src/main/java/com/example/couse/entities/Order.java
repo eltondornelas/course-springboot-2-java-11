39c82d320946b944e4102324b05d82c3412d5eff
Elton Dornelas <eltondornelas@gmail.com>
2019-09-18 14:40:46 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/example/couse/entities/Order.java
+++ src/main/java/com/example/couse/entities/Order.java
@@ -2,6 +2,8 @@ package com.example.couse.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.example.couse.entities.enums.OrderStatus;
@@ -35,6 +38,9 @@ public class Order implements Serializable {
 	private User client;
 	// nome que está no UML
 
+	@OneToMany(mappedBy = "id.order")	
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Order() {
 
 	}
@@ -81,6 +87,10 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/com/example/couse/entities/OrderItem.java
+++ src/main/java/com/example/couse/entities/OrderItem.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.example.couse.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order_item")
@@ -15,8 +16,9 @@ public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	//esse que será o identificador dele
+	//tem que instanciar se não quebra o programa
 	
 	private Integer quantity;
 	private Double price;
@@ -33,6 +35,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
