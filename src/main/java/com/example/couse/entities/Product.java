2ba091b20ade5f650b6dcff38318a03c6748177b
Elton Dornelas <eltondornelas@gmail.com>
2019-09-18 13:43:09 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/example/couse/entities/Product.java
+++ src/main/java/com/example/couse/entities/Product.java
@@ -8,32 +8,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //autoincremento
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // autoincremento
 	private Long id;
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient //impede que o JPA tente interpretar...
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	//set representa um conjunto, garante que não tenha 1 produto com mais de uma categoria
-	//instanciamos para garantir que não seja nula, ela vai iniciar vazia
-	//Set é interface ele não pode ser instanciado, precisa de uma classe correspondente a interface, por isso usamos o HashSet
-	
+	// set representa um conjunto, garante que não tenha 1 produto com mais de uma
+	// categoria
+	// instanciamos para garantir que não seja nula, ela vai iniciar vazia
+	// Set é interface ele não pode ser instanciado, precisa de uma classe
+	// correspondente a interface, por isso usamos o HashSet
+
 	public Product() {
-		
+
 	}
+
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
@@ -41,42 +47,53 @@ public class Product implements Serializable {
 		this.description = description;
 		this.price = price;
 		this.imgUrl = imgUrl;
-		//como já estou instanciando a coleção, não é necessário colocar no construtor
+		// como já estou instanciando a coleção, não é necessário colocar no construtor
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public Double getPrice() {
 		return price;
 	}
+
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
 	public String getImgUrl() {
 		return imgUrl;
 	}
+
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
+
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -84,7 +101,7 @@ public class Product implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -101,5 +118,5 @@ public class Product implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 }
